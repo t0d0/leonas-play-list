bc3f03129c34d50adddeb693f482a51dcf114daf
t0d0 <hiroya@t0d0.jp>
2023-02-18 02:31:32 +0900

    自動バックアップをコンテナに導入したい。

--- lpl_const/config.py
+++ lpl_const/config.py
@@ -4,13 +4,12 @@
 port: int = 8899
 """サーバーの起動ポート"""
 mongo_host: str = 'mongo'
-# mongo_host: str = 'localhost'
-
 """mongoのホスト名"""
 mongo_port: str = 27017
 """mongoのポート番号"""
 mongo_backup_path: str = ""
 """mongoのバックアップファイル出力先"""
 
-channel_json_file_path: str = "/apps/channel_videos.json"
+channel_json_file_path: str = '/apps/channel_videos.json'
 """公式チャンネルのコンテンツ一覧ファイルのパス"""
+view_application_name: str = "Leona'sPlayList"
